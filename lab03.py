76e8ba170f0337bca1c91216a7578f18fb787480
RosyBoby <146629156+RosyBoby@users.noreply.github.com>
2023-10-23 16:41:41 +0530

    Add files via upload

---
+++ lab03.py
@@ -0,0 +1,13 @@
+my_line="this is 'Code'"
+print(my_line)
+
+first_number = input ("Enter you first number\n")
+second_number = input ("Enter you second number\n")
+x = int(first_number)
+y= int(second_number)
+print("Sum is :" ,x +y ,end ='\n')
+print("Difference is :" ,x -y, end ='\n')
+print("Product is :" ,x *y , end ='\n')
+print("Division is :" ,x /y, end = '\n')
+
+
---
+++ lab04.py
@@ -0,0 +1,6 @@
+my_string ="This is Pramod from Testing Accademy"
+print(len(my_string))
+ggg ="Suryanarayanan"
+print(ggg[5:8])
+print(ggg[5:2])
+print(r"C:\rosy\name\odd")
