474f38347883efc843c4d6e732bca7d9fd9dcda3
RosyBoby <146629156+RosyBoby@users.noreply.github.com>
2023-10-23 17:08:16 +0530

    Add files via upload

---
+++ Task_on_6_10_23.py
@@ -0,0 +1,23 @@
+#Task 2:-
+'''Take an input from a user and print the table
+How to use the print with formatting f
+print(f''), How can print the same?'''
+
+#exampe
+# name = 'Tushar'
+# age = 23
+# print(f"Hello, My name is {name} and I'm {age} years old.")
+
+yyy= input ("Enter the number for which you want to see the Multiplication Table :\n")
+kkk= int(yyy)
+print (f'The table for {kkk} is :\n')
+print(f'{kkk} * 1 ={kkk*1}')
+print(f'{kkk} * 2 ={kkk*2}')
+print(f'{kkk} * 3 ={kkk*3}')
+print(f'{kkk} * 4 ={kkk*4}')
+print(f'{kkk} * 5 ={kkk*5}')
+print(f'{kkk} * 6 ={kkk*6}')
+print(f'{kkk} * 7 ={kkk*7}')
+print(f'{kkk} * 8 ={kkk*8}')
+print(f'{kkk} * 9 ={kkk*9}')
+print(f'{kkk} * 10 ={kkk*10}')
