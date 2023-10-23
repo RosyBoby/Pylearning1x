751753ff22138cbc12091545e8e60d19c2137f5b
RosyBoby <146629156+RosyBoby@users.noreply.github.com>
2023-10-23 12:29:01 +0530

    Add files via upload

---
+++ lab02.py
@@ -0,0 +1,29 @@
+print ("Hello World")
+name='Rosy Roy'
+age =30
+DOB = '1993-09-20'
+print(DOB)
+a = max (9,1)
+print(a)
+
+#import keyword
+#print(keyword.kwlist)
+
+#['False', 'None', 'True', 'and', 'as', 'assert',
+# 'async', 'await', 'break', 'class', 'continue', 'def',
+# 'del', 'elif', 'else', 'except', 'finally', 'for', 'from',
+# 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal',
+# 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+#variablename=variablevalue
+
+e = 34.55
+print(type(e))
+# ooo = input ("Enter you first name\n")
+# kkk = input ("Enter you last name\n ")
+# print (ooo,kkk,sep = "_",end = '\t')
+
+uuu = input ("Enter you  name\n")
+print ("You are welcome to the Python Class :",uuu )
+print ("You are welcome:",uuu, "to the Python Class", end ='\n' )
+
